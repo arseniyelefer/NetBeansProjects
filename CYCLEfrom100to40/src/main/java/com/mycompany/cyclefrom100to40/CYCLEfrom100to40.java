0876627c2e2dd9f8aeb34c008bc42c2c41355304
Arseniy <arseniy.elefer@gmail.com>
2022-11-04 17:48:37 +0300

    loop from 100 to 40

---
+++ CYCLEfrom100to40/src/main/java/com/mycompany/cyclefrom100to40/CYCLEfrom100to40.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.cyclefrom100to40;
+
+/**
+ *
+ * @author Lenovo x270
+ */
+public class CYCLEfrom100to40{
+    
+    public static void main (String[] args){
+        System.out.println("цикл от 100 до 40 ");
+        for (int num=100; num>=40; num--)
+                System.out.println(num + " ");
+    }
+}
